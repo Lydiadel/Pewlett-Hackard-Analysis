9ddddbd20d3fabf27b46a5c5aadd0a78e0e57844
Lydiadel <lydia101-lidi@hotmail.com>
2022-02-21 02:37:32 -0600

    Remove duplicated values and create unique titles

--- Employee_Challenge.sql
+++ Employee_Challenge.sql
@@ -1,10 +1,28 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_Date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN title as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+SELECT * FROM retirement_titles;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) 
+	rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles 
+FROM retirement_titles as rt
+WHERE to_date = '9999-01-01'
+ORDER BY rt.emp_no, rt.to_date DESC;
+SELECT * FROM unique_titles;
